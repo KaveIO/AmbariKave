0bc2d641dbfcf783fed9f9ccbfeef49778f08a6c
rwlambert <lambert.rob@kpmg.nl>
2015-08-20 09:00:39 +0000

    adapt storm to use clusterHostInfo, let's see if it works ...

--- src/HDP/2.2.KAVE/services/STORMSD/package/scripts/params.py
+++ src/HDP/2.2.KAVE/services/STORMSD/package/scripts/params.py
@@ -20,9 +20,12 @@ from resource_management import *
 config = Script.get_config()
 storm_conf_file = "/usr/local/storm/conf/storm.yaml"
 
-storm_zookeeper_servers = config['configurations']['stormsd']['stormsd.zookeeper.servers'].replace(", "," ").replace(","," ").split()
-nimbus_host = config['configurations']['stormsd']['stormsd.nimbus.host']
-drpc_servers = config['configurations']['stormsd']['stormsd.drpc.servers'].replace(", "," ").replace(","," ").split()
+storm_zookeeper_servers = default("/clusterHostInfo/zookeeper_server_hosts", None)#config['configurations']['stormsd']['stormsd.zookeeper.servers'].replace(", "," ").replace(","," ").split()
+nimbus_host = default("/clusterHostInfo/nimbus_sd_master_hosts", [None])[0]
+drpc_servers = default("/clusterHostInfo/stormsd_drpc_server_hosts", None)#config['configurations']['stormsd']['stormsd.drpc.servers'].replace(", "," ").replace(","," ").split()
+
+if None in [storm_zookeeper_servers, nimbus_host, drpc_servers]:
+    raise NameError("Could not find required services from clusterHostInfo : "+str(config['clusterHostInfo']))
 
 storm_zookeeper_port = default('configurations/stormsd/stormsd.zookeeper.port', "2181")
 nimbus_childopts = default('configurations/stormsd/stormsd.nimbus.childopts', '-Xmx1024m -Djava.net.preferIPv4Stack=true')

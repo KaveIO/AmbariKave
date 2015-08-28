3090e8627e36ecd8c01d5b44ab1c2d8a5becd7cd
hoekstra.maarten@kpmg.nl <hoekstra.maarten@kpmg.nl>
2015-08-28 11:36:43 +0200

    Moved KaveToolbox from pre version to release version.

--- src/HDP/2.2.KAVE/services/KAVETOOLBOX/package/scripts/params.py
+++ src/HDP/2.2.KAVE/services/KAVETOOLBOX/package/scripts/params.py
@@ -25,7 +25,7 @@ config = Script.get_config()
 hostname = config["hostname"]
 
 top_dir = default("configurations/kavetoolbox/top_dir", "/opt/")
-releaseversion = default('configurations/kavetoolbox/releaseversion', "1.3-Beta-Pre")
+releaseversion = default('configurations/kavetoolbox/releaseversion', "1.3-Beta")
 alternative_download = default('configurations/kavetoolbox/alternative_download', "")
 ignore_missing_groups = default('configurations/kavetoolbox/ignore_missing_groups', "False")
 ignore_missing_groups = kc.trueorfalse(ignore_missing_groups)

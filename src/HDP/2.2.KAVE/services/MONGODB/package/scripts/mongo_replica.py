9b9d345ef50058c953d9b1b88924c9463812b544
rsandstroem <rsandstroem@kpmg.com>
2015-10-28 11:26:07 +0100

    Add replica python

---
+++ src/HDP/2.2.KAVE/services/MONGODB/package/scripts/mongo_replica.py
@@ -0,0 +1,69 @@
+##############################################################################
+#
+# Copyright 2015 KPMG N.V. (unless otherwise stated)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+##############################################################################
+import os
+
+from resource_management import *
+from mongo_base import MongoBase
+
+class MongoMaster(MongoBase):
+    mongo_packages=['mongodb-org']
+
+    def install(self, env):
+        import params
+        env.set_params(params)
+        self.installMongo(env)
+        self.configure(env)
+
+    def configure(self,env):
+        import params
+        env.set_params(params)
+        self.configureMongo(env)
+
+    def start(self, env):
+        print "start mongodb"
+        self.configure(env)
+        # TODO: Ambari 2.0 method should be replacing the below call
+        # since Ambari 1.7.3 execute method never returns the control to script
+        # So, we use nohup to detacht he start process, and we also need to redirect all the input and output
+        Execute('service mongod --port 27018 --replSet rs0 start  2> /dev/null > /dev/null < /dev/null &')
+
+    def stop(self, env):
+        print "stop services.."
+        Execute('service mongod stop')
+
+    def restart(self, env):
+        """MongoDB service is not very quick to shut down.
+        Restarting without waiting for stop is causes quite a few problems along the lines of 'cannot bind to port blah already in use.'
+        A specific restart method is useful to ensure ambari waits between start and stop.
+        """
+        #Stop first
+        print "restart mongodb"
+        self.stop(env)
+        #3 seconds seems long enough in most cases to wait for the stop
+        import time
+        time.sleep(3)
+        #now configure and start normally
+        self.start(env)
+
+    def status(self, env):
+        print "checking status..."
+        Execute('service mongod status')
+
+
+if __name__ == "__main__":
+    MongoMaster().execute()

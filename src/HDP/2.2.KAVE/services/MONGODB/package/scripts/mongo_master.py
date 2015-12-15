59c43e57b4bbac565b53975d72542a8b6f7c7a38
rsandstroem <rsandstroem@kpmg.com>
2015-12-15 13:55:44 +0100

    hard coded first replica as primary

--- src/HDP/2.2.KAVE/services/MONGODB/package/scripts/mongo_master.py
+++ src/HDP/2.2.KAVE/services/MONGODB/package/scripts/mongo_master.py
@@ -76,10 +76,10 @@ class MongoMaster(MongoBase):
                 import time
                 try:
                     time.sleep(10)
-                    Execute('mongo --host ', mongo_hosts[0], ' < /tmp/replicaset_conf.js')
+                    Execute('mongo --host replica-001 < /tmp/replicaset_conf.js')
                 except:
                     time.sleep(60)
-                    Execute('mongo --host ', mongo_hosts[1], ' < /tmp/replicaset_conf.js')
+                    Execute('mongo --host replica-001 < /tmp/replicaset_conf.js')
 
     def stop(self, env):
         print "stop services.."

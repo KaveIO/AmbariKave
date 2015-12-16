db9116fd275f547c9f5a39492035a08fea0d6ba9
rsandstroem <rsandstroem@kpmg.com>
2015-12-16 12:07:44 +0100

    debug

--- src/HDP/2.2.KAVE/services/MONGODB/package/scripts/mongo_master.py
+++ src/HDP/2.2.KAVE/services/MONGODB/package/scripts/mongo_master.py
@@ -59,7 +59,7 @@ class MongoMaster(MongoBase):
                 # insert the document into the primary worker node to start replication
                 import time
                 time.sleep(60)
-                Execute('mongo < /tmp/replicaset_conf.js >! /tmp/replicaset_debug.txt 2>&1&')
+                Execute('mongo < /tmp/replicaset_conf.js > /tmp/replicaset_debug.txt 2>&1&')
                 import sys
                 print >> sys.stderr, "Deliberate failure"
                 sys.exit(1)

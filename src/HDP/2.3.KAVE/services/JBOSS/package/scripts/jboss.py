ee4a44d677ec7f71aea4751676ad96b05543dbbe
anaskar <anaskar@anaskar_dev_box.kave.io>
2016-03-09 06:01:34 +0000

    removed commented out code to make code more cleaner

--- src/HDP/2.3.KAVE/services/JBOSS/package/scripts/jboss.py
+++ src/HDP/2.3.KAVE/services/JBOSS/package/scripts/jboss.py
@@ -79,7 +79,6 @@ class Jboss(Script):
         env.set_params(params)
 
         File(params.jboss_conf_file,
-             # content=Template("standalone.xml"),
              content=InlineTemplate(params.jbossconfig),
              mode=0644
              )

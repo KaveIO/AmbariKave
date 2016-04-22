bc696838d67f716ee007909ee7aaa71641d68bf4
rlambert <lambert.rob@kpmg.nl>
2016-04-22 15:33:01 +0000

    Moved to new format for AmbariMetrics

--- src/HDP/2.4.KAVE/services/stack_advisor.py
+++ src/HDP/2.4.KAVE/services/stack_advisor.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env ambari-python-wrap
-"""
-Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file
-to you under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-
+##############################################################################
+#
+# Copyright 2016 KPMG N.V. (unless otherwise stated)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+##############################################################################
 
 class HDP24KAVEStackAdvisor(HDP24StackAdvisor):
 
@@ -33,7 +32,7 @@ class HDP24KAVEStackAdvisor(HDP24StackAdvisor):
     def getServiceConfigurationValidators(self):
         parentValidators = super(HDP24KAVEStackAdvisor, self).getServiceConfigurationValidators()
         childValidators = {
-            "FREEIPA": ["freeipa", self.validateFreeIPAConfigurations]
+            "FREEIPA": {"freeipa", self.validateFreeIPAConfigurations}
         }
         parentValidators.update(childValidators)
         return parentValidators

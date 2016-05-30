b15f364c24069a92fd37fbeadff94e1dfe3367e1
rlambert <lambert.rob@kpmg.nl>
2016-05-30 11:15:16 +0000

    still missing some centos7 change in latest commit for FreeIPA!

--- src/HDP/2.4.KAVE/services/FREEIPA/package/scripts/freeipa_server.py
+++ src/HDP/2.4.KAVE/services/FREEIPA/package/scripts/freeipa_server.py
@@ -53,11 +53,11 @@ class FreeipaServer(Script):
         # of some anoying centos6.5 problems. The underling installer uses an
         # outdated method for the dogtag system which fails.
         # however, on centos7, this option does not exist!
-        if tos in ["centos6"]:
+        if tos.lower() in ["centos6"]:
             install_command += " --selfsign"
 
         if params.install_with_dns:
-            if tos in ["centos7"]:
+            if tos.lower() in ["centos7"]:
                 Package("ipa-server-dns")
             install_command += ' --setup-dns --domain="%s"' % params.domain
             if params.forwarders:

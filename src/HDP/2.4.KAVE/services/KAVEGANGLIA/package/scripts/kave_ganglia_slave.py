fd58cc8b96a272b6a20d7847e83a030342175cac
manishkumar7003 <manishkumar7@kpmg.com>
2016-06-10 12:33:35 +0000

    added ganglia slave in KAVEGANGLIA serivce

---
+++ src/HDP/2.4.KAVE/services/KAVEGANGLIA/package/scripts/kave_ganglia_slave.py
@@ -0,0 +1,52 @@
+##############################################################################
+#
+# Copyright 2016 KPMG Advisory N.V. (unless otherwise stated)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+##############################################################################
+import os
+
+from resource_management import *
+import kavecommon as kc
+
+
+class KaveGangliaSlave(Script):
+
+    def install(self, env):
+        import params
+
+        env.set_params(params)
+        self.install_packages(env)
+        self.configure(env)
+
+    def configure(self, env):
+        import params
+        import kavecommon as kc
+
+        env.set_params(params)
+        Execute('chkconfig gmond on')
+
+    def start(self, env):
+        self.configure(env)
+        Execute("service gmond start")
+
+    def stop(self, env):
+        Execute("service gmond stop")
+
+    def status(self, env):
+        print Execute("service gmond status")
+
+
+if __name__ == "__main__":
+    KaveGangliaSlave().execute()

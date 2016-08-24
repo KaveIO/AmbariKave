d61cc83722ed47cc31417cb45b68b11aa6c71f94
rlambert <lambert.rob@kpmg.nl>
2016-08-24 14:58:50 +0000

    add parallelization to clean script

--- dev/clean.sh
+++ dev/clean.sh
@@ -16,6 +16,16 @@
 #   limitations under the License.
 #
 ##############################################################################
+#
+# Script to completely clean an ambari installation
+# only cleans ambari itself, does nothing with the services that
+# ambari may have installed. Accepts one arguement, the name of the pdsh group
+# associated to the whole cluster, if supplied will use pdsh to remove agents
+# from all the machines in the cluster as well as the local machine
+#
+
+if
+
 echo "This will completely destroy your local ambari installation and database."
 read -p "Are you sure? " -n 1 -r
 echo    # (optional) move to a new line
@@ -27,12 +37,56 @@ else
 	exit 0
 fi
 
-ambari-server stop
-ambari-agent stop
-sleep 5
-yum -y erase ambari-agent ambari-server
-su - postgres bash -c 'psql -c "drop database ambari"; psql -c "drop database ambarirca";'
-rm -rf /var/lib/ambari-server/*
-rm -rf /var/lib/ambari-agent/*
-rm -rf /etc/yum.repos.d/ambari.repo
 
+cluster=$1
+
+clustercmd=""
+
+if [ ! -z "$1" ]; then
+
+	if ! type pdsh >/dev/null 2>/dev/null ; then
+		echo 'no pdsh installed'
+		exit 1
+	fi
+
+	pdsh_version=`pdsh -V | head -n 1 | awk '{print $1}' | awk -F '-' '{print $2}'`
+	pdsh_groups=`pdsh -h 2>&1 | tail -n 10 | grep groupname`
+
+	if [[  ${pdsh_version} < "2.27" ]]; then
+	        echo "pdsh version >= 2.27 needed for this script, you have"
+	        pdsh -V
+	        exit 1
+	fi
+
+	if [[  ${pdsh_groups} == *"-g groupname"* ]]; then
+	        true
+	else
+	        pdsh -h
+	        echo "No pdsh groups detected, install newer version or install pdsh-mod-dshgroup"
+	        exit 1
+	fi
+
+	if [ ! -e ~/.dsh/group/$1 ]; then
+		echo "no dsh group called "$1" found, did you type the cluster name in wrongly?"
+		echo "deploy_from_blueprint makes groups named after the cluster, if you didn't use that this won't work either"
+		echo "you can also make the groups yourself, with all nodes"
+		exit 1
+	fi
+
+	clustercmd="pdsh -g $cluster "
+
+fi
+
+
+ambari-server stop
+${clustercmd}ambari-agent stop
+sleep 5
+yum -y erase ambari-server
+${clustercmd}yum -y erase ambari-agent
+su - postgres bash -c 'psql -c "drop role ambari; drop database ambari"; psql -c "drop database ambarirca";'
+rm -f .pgpass
+rm -rf /var/lib/ambari-server/*
+rm -rf /etc/ambari-server/*
+${clustercmd}rm -rf /var/lib/ambari-agent/*
+${clustercmd}rm -rf /etc/ambari-agent/*
+${clustercmd}rm -rf /etc/yum.repos.d/ambari.repo

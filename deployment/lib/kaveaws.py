397e2681106a18720116b6ccb8c496395600fd96
rlambert <lambert.rob@kpmg.nl>
2016-08-31 08:19:41 +0000

    centos7 default on centos7 branch

--- deployment/lib/kaveaws.py
+++ deployment/lib/kaveaws.py
@@ -37,7 +37,7 @@ import Queue
 # Centos6 has the username root, Centos7 has the username 'centos'
 default_usernamedict = {"Centos6": "root", "Centos7": 'centos',
                         "Redhat7": 'ec2-user', "Ubuntu14": 'ubuntu'}
-default_os = "Redhat7"
+default_os = "Centos7"
 
 
 def testaws():

70f79cb0f9edf9a3bd40628cceac3115e9d24783
rlambert <lambert.rob@kpmg.nl>
2016-09-13 15:39:37 +0000

    "No errors were found" message suppress

--- deployment/lib/kavedeploy.py
+++ deployment/lib/kavedeploy.py
@@ -790,6 +790,7 @@ def wait_for_ambari(ambari, maxrounds=10, check_inst=None):
                     # ignore errors with mirrors
                     cat = cat.replace("[Errno 14] HTTP Error 404 - Not Found".lower(), '')
                     cat = cat.replace("[Errno 14] HTTP Error 503 - Service Unavailable".lower(), '')
+                    cat = cat.replace("No errors were found.".lower(), '')
                     if "error" in cat or "exception" in cat or "failed" in cat:
                         raise SystemError("Failure in ambari server start server detected!")
         except ShellExecuteError:

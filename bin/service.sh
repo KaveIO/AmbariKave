3eb0413730d393d3dd101eeeab1639a3d2cd4593
abhiramip1 <abhiramip1@kpmg.com>
2017-01-12 10:24:44 +0000

    Added draft code for Airflow integartion

--- bin/service.sh
+++ bin/service.sh
@@ -145,6 +145,9 @@ function runConfigureService {
     KAVENAGIOS)
 	buildConfiguration kavenagios nagios_admin_password
       ;;
+    AIRFLOW)
+	buildConfiguration airflow AMBARI_ADMIN AMBARI_ADMIN_PASS
+      ;;
   esac
 }
 
@@ -190,6 +193,7 @@ function runList {
   echo "	STORMSD_DRPC_SERVER"
   echo "	STORMSD_CLIENT"
   echo "	TWIKI"
+  echo "	AIRFLOW"
   echo ""
 }
 
@@ -315,6 +319,8 @@ elif [ $service = "METRICS" ]; then
 elif [ $service = "METRICS_MONITOR" ]; then
   service="AMBARI_METRICS"
   component="METRICS_MONITOR"
+elif [ $service = "AIRFLOW" ]; then
+  component="AIRFLOW_SERVER"
 else
   echo "ERROR: unknown service caught: $service"
   runHelp

98254f88e398bba8fe9772ff25df5423508bc78c
abhiramip1 <abhiramip1@kpmg.com>
2017-02-03 09:47:34 +0000

    Airflow python script, params, xml files and blueprint created

---
+++ src/KAVE/services/AIRFLOW/package/scripts/airflow.py
@@ -0,0 +1,99 @@
+##############################################################################
+#
+# Copyright 2016 KPMG Advisory N.V. (unless otherwise stated)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+##############################################################################
+import os
+
+from resource_management import *
+#from kavecommon import ApacheScript
+#from resource_management.core.exceptions import ComponentIsNotRunning
+
+
+class Airflow():
+    # status file is needed to know if this service was started, stores the name of the index file
+    #status_file = '/etc/kave/kavelanding_started'
+    airflow_config_path = "/root/airflow/airflow.cfg"
+    airflow_webserver_pidfile_path = "/root/airflow/airflow-webserver.pid"
+    airflow_config_path = "/etc/httpd/conf.d/ganglia.conf"
+
+
+    def install(self, env):
+        print "Installing Airflow"
+        import params
+        import kavecommon as kc
+        import os
+        super(Airflow, self).install(env)
+        kc.install_epel()
+        Execute('curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"')
+        Package('python get-pip.py')
+        Execute('yum -y update')
+        Execute('sudo yum install postgresql-devel y')
+        Execute('yum install python-devel mysql-devel y')
+
+        #Package('python-devel')
+        #Package('python-pip')
+
+        Execute('sudo yum -y install gcc gcc-c++ libffi-devel mariadb-devel cyrus-sasl-devel')
+
+        Execute('pip install airflow')
+
+        self.configure(env)
+
+    def configure(self, env):
+        import params
+        import os
+        import kavecommon as kc
+        env.set_params(params)
+        #self.write_html(env)
+        super(Airflow, self).configure(env)
+
+    def start(self, env):
+        if not os.path.exists(os.path.dirname(self.status_file)):
+            os.makedirs(os.path.dirname(self.status_file))
+        import params
+        import os
+        Execute('airflow initdb')
+        Execute('airflow webserver -p 8080')
+        Execute('airflow scheduler')
+
+        self.configure(env)
+        super(KaveLanding, self).start(env)
+        # Write the location of the index file into the status file
+        if os.path.exists(params.www_folder + '/index.html'):
+            with open(self.status_file, 'w') as fp:
+                fp.write(params.www_folder + '/index.html')
+
+    def stop(self, env):
+        import params
+        import os
+
+        Execute('cat $AIRFLOW_HOME/airflow-webserver.pid | xargs kill -9')
+
+
+    def status(self, env):
+        # Read from the status file, and check the index exists
+        if not os.path.exists(self.status_file):
+            raise ComponentIsNotRunning()
+        klfile = None
+        with open(self.status_file) as fp:
+            klfile = fp.read().split()[0].strip()
+        if len(klfile) < 5 or (not os.path.exists(klfile)):
+            raise ComponentIsNotRunning()
+        super(KaveLanding, self).status(env)
+
+
+if __name__ == "__main__":
+    KaveLanding().execute()
---
+++ src/KAVE/services/AIRFLOW/package/scripts/params.py
@@ -0,0 +1,53 @@
+##############################################################################
+#
+# Copyright 2016 KPMG Advisory N.V. (unless otherwise stated)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+##############################################################################
+from resource_management import *
+from resource_management.core.system import System
+import os
+import kavecommon as kc
+
+
+config = Script.get_config()
+
+hostname = config["hostname"]
+
+short_host = config["hostname"].split('.')[0]
+
+AMBARI_ADMIN = config['configurations']['airflow']['AMBARI_ADMIN']
+AMBARI_ADMIN_PASS = config['configurations']['airflow']['AMBARI_ADMIN_PASS']
+Logger.sensitive_strings[AMBARI_ADMIN_PASS] = "[PROTECTED]"
+
+AMBARI_SERVER = default("/clusterHostInfo/ambari_server_host", ['ambari'])[0]
+# default('configurations/kavelanding/AMBARI_SERVER','ambari')
+www_folder = kc.default('configurations/kavelanding/www_folder', '/var/www/html/', kc.is_valid_directory)
+
+PORT = kc.default('configurations/airflow/PORT', '80', kc.is_valid_port)
+AMBARI_SHORT_HOST = AMBARI_SERVER.split('.')[0]
+servername = kc.default('configurations/airflow/servername', hostname, kc.is_valid_hostname)
+if servername == "default":
+    servername = hostname
+
+
+#template_000_default = default('configurations/kavelanding/template_000_default', """# Created automatically with Ambari
+# All manual changes will be undone in the case of a server restart
+# Edit the template through the Ambari interface instead
+#TraceEnable Off
+#RequestHeader unset Proxy early
+#Listen {{PORT}}
+#ServerName "{{servername}}"
+#DocumentRoot "{{www_folder}}"
+#""")

1510ddb22d99876b64129165dc33151c42d56683
Kalin Sheytanov <ksheytanov@intracol.com>
2017-04-19 10:02:38 +0300

    Removing KAVEGANGLIA, WILDFIGY and MONGODB from service.sh
    These three have blueprints defined and there is no mneed to test them
    with service.sh as well.

--- tests/service/all.py
+++ tests/service/all.py
@@ -26,8 +26,8 @@ import os
 mods = [test_kavetoolbox_head, remote_service_with_servicesh, remote_service_with_blueprint]
 
 # Ignore services which do not have a working default configuration, or have default ports which conflict with 8080
-ignore_services = ["GITLAB", "TWIKI", "KAVENAGIOS", "FREEIPA", "JENKINS", "ARCHIVA",
-                   "JBOSS", "KAVELANDING", "STORMSD", "SONARQUBE", "MAIL"]
+ignore_services = ["GITLAB", "TWIKI", "KAVENAGIOS", "KAVEGANGLIA", "FREEIPA", "JENKINS", "ARCHIVA",
+                   "JBOSS", "WILDFLY", "KAVELANDING", "STORMSD", "SONARQUBE", "MAIL", "MONGODB"]
 # Gitlab cannot be installed on the ambari server because of the conflicting SQL databases
 
 services = [s for s, ds in base.find_services() if s not in ignore_services]

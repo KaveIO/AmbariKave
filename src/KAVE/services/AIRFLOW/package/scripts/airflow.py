310b1a86f41ad8562713a653ab29fb0fbca83cb6
Kalin Sheytanov <ksheytanov@intracol.com>
2017-06-12 15:26:13 +0300

    replacing yum command

--- src/KAVE/services/AIRFLOW/package/scripts/airflow.py
+++ src/KAVE/services/AIRFLOW/package/scripts/airflow.py
@@ -49,7 +49,7 @@ class Airflow(kc.ApacheScript):
 
         Execute('curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"')
         Execute('python get-pip.py')
-        Execute('yum -y update')
+        Execute('yum clean all')
         Execute('sudo yum install -y postgresql-devel python-devel mysql-devel')
         Execute('sudo yum -y install gcc gcc-c++ libffi-devel mariadb-devel cyrus-sasl-devel')
         Execute('pip install -U pip setuptools')

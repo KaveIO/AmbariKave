cb22a27602c7cbac45eb694a56b740edd05759c0
Ralitsa Todorova <rtodorova@intracol.com>
2017-09-11 08:57:01 -0400

    Disable version disclosure for Apache
    Add properties that disable version disclosure for all services that rely on Apache
    
    Signed-off-by: Ralitsa Todorova <rtodorova@intracol.com>

--- src/KAVE/services/KAVEGANGLIA/package/scripts/params.py
+++ src/KAVE/services/KAVEGANGLIA/package/scripts/params.py
@@ -72,6 +72,8 @@ template_000_default = default('configurations/kaveganglia/template_000_default'
 # Edit the template through the Ambari interface instead
 TraceEnable Off
 RequestHeader unset Proxy early
+ServerSignature Off
+ServerTokens Prod
 Listen {{PORT}}
 ServerName "{{servername}}"
 DocumentRoot "{{www_folder}}"

b4c49d8a8be7a0194864f42a4d74fefa79413f67
Kalin Sheytanov <ksheytanov@intracol.com>
2017-10-05 11:12:47 +0300

    removing not needed variables

--- deployment/cloudbreak/setup_cloudbreak_kavepatch_ambari.sh
+++ deployment/cloudbreak/setup_cloudbreak_kavepatch_ambari.sh
@@ -7,16 +7,13 @@
 KAVE_VERSION=3.3
 HDP_STACK=2.6
 KAVE_STACK=$HDP_STACK.$KAVE_VERSION.KAVE
-DEFAULT_STACK_ADVISOR=HDP26StackAdvisor
-PARENT_STACK_ADVISOR=HDP25StackAdvisor
-KAVE_STACK_ADVISOR=HDP2633KAVEStackAdvisor
 
 yum -y install wget curl tar zip unzip gzip python
 wget http://repos:kaverepos@repos.kave.io/noarch/AmbariKave/$KAVE_VERSION-Beta/ambarikave-package-$KAVE_VERSION-Beta.tar.gz
 tar -xzf ambarikave-package-$KAVE_VERSION-Beta.tar.gz -C /var/lib \
 --transform "s/$KAVE_STACK/$HDP_STACK/" \
 --exclude ambari-server/resources/stacks/HDP/$KAVE_STACK/metainfo.xml \
---exclude ambari-server/resources/stacks/HDP/$KAVE_STACK/services/stack_advisor.py
+--exclude ambari-server/resources/stacks/HDP/$KAVE_STACK/services/stack_advisor.py \
 --exclude repoinfo.xml
 systemctl stop ambari-server
 systemctl start ambari-server

cdf9f2f5c41516c847b606cd6cf5c65c5d52313a
Kalin Sheytanov <ksheytanov@intracol.com>
2017-10-05 11:11:41 +0300

    kave stack advisor removal
    excluding our stack advisor as it is only about freeipa and it
    overwrites the HDP 2.6 one, which is not good.

--- deployment/cloudbreak/setup_cloudbreak_kavepatch_ambari.sh
+++ deployment/cloudbreak/setup_cloudbreak_kavepatch_ambari.sh
@@ -4,16 +4,19 @@
 #Log at /var/log/recipes/pre-kaveprepatch.log
 
 #At every release, please update these variables.
-KAVE_VERSION=3.1-Beta
-HDP_STACK=2.5
-KAVE_STACK=2.5.3.1.KAVE
-DEFAULT_STACK_ADVISOR=HDP25StackAdvisor
-PARENT_STACK_ADVISOR=HDP24StackAdvisor
-KAVE_STACK_ADVISOR=HDP2530KAVEStackAdvisor
+KAVE_VERSION=3.3
+HDP_STACK=2.6
+KAVE_STACK=$HDP_STACK.$KAVE_VERSION.KAVE
+DEFAULT_STACK_ADVISOR=HDP26StackAdvisor
+PARENT_STACK_ADVISOR=HDP25StackAdvisor
+KAVE_STACK_ADVISOR=HDP2633KAVEStackAdvisor
 
 yum -y install wget curl tar zip unzip gzip python
-wget http://repos:kaverepos@repos.kave.io/noarch/AmbariKave/$KAVE_VERSION/ambarikave-package-$KAVE_VERSION.tar.gz
-tar -xzf ambarikave-package-$KAVE_VERSION.tar.gz -C /var/lib --transform "s/$KAVE_STACK/$HDP_STACK/" --exclude ambari-server/resources/stacks/HDP/$KAVE_STACK/metainfo.xml --exclude repoinfo.xml
-sed -i -e "s/$DEFAULT_STACK_ADVISOR/$PARENT_STACK_ADVISOR/" -e "s/$KAVE_STACK_ADVISOR/$DEFAULT_STACK_ADVISOR/" /var/lib/ambari-server/resources/stacks/HDP/$HDP_STACK/services/stack_advisor.py
-ambari-server stop
-ambari-server start
+wget http://repos:kaverepos@repos.kave.io/noarch/AmbariKave/$KAVE_VERSION-Beta/ambarikave-package-$KAVE_VERSION-Beta.tar.gz
+tar -xzf ambarikave-package-$KAVE_VERSION-Beta.tar.gz -C /var/lib \
+--transform "s/$KAVE_STACK/$HDP_STACK/" \
+--exclude ambari-server/resources/stacks/HDP/$KAVE_STACK/metainfo.xml \
+--exclude ambari-server/resources/stacks/HDP/$KAVE_STACK/services/stack_advisor.py
+--exclude repoinfo.xml
+systemctl stop ambari-server
+systemctl start ambari-server

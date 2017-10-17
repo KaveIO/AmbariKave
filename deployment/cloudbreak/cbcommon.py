e4c5a260fda48cd48bb37d35241c9b30282e9502
Ralitsa Todorova <rtodorova@intracol.com>
2017-10-17 10:33:34 -0400

    ABK-1330: Implement authentication - getting a token for the API
    Signed-off-by: Ralitsa Todorova <rtodorova@intracol.com>

---
+++ deployment/cloudbreak/cbcommon.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import params
+import urlparse
+
+def get_access_token():
+
+    token=None
+    url = params.cb_http_url + ':'+ str(params.uaa_port) + '/oauth/authorize?response_type=token&client_id=cloudbreak_shell&ope.0=openid&source=login&redirect_uri=http://cloudbreak.shell'
+    headers = {'Accept': 'application/x-www-form-urlencoded', 'Content-type':'application/x-www-form-urlencoded'}
+    data={'credentials':'{"username": "' + params.cb_username + '", "password": "' + params.cb_password + '"}' }
+
+    try:
+        auth=requests.post(url, data=data, headers=headers, allow_redirects=False)
+        location_header = urlparse.urlparse(auth.headers["Location"])
+        parsed_params = urlparse.parse_qsl(location_header.fragment)
+        for item in parsed_params:
+            if item[0]=="access_token":
+                token=item[1]
+        return token
+    except Exception as e:
+        print "Error getting Cloudbreak access token: ", e
---
+++ deployment/cloudbreak/params.py
@@ -0,0 +1,5 @@
+cb_http_url="http://40.83.253.211"
+cb_https_url="https://40.83.253.211"
+uaa_port=8089
+cb_username="admin@example.com"
+cb_password="KavePassword01"

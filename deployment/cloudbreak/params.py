6cef3d21ffaa98484473a8ca2c8a23200a7bc2a4
Ralitsa Todorova <rtodorova@intracol.com>
2017-11-02 12:50:06 -0400

    Change cb_url with the new Cloudbreak machine IP
    Signed-off-by: Ralitsa Todorova <rtodorova@intracol.com>

--- deployment/cloudbreak/params.py
+++ deployment/cloudbreak/params.py
@@ -1,6 +1,6 @@
 kave_version="33-beta"
-cb_http_url="http://40.83.253.211"
-cb_https_url="https://40.83.253.211"
+cb_http_url="http://13.64.195.21"
+cb_https_url="https://13.64.195.21"
 uaa_port=8089
 cb_username="admin@example.com"
 cb_password="KavePassword01"

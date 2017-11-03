df03f25475fdcda67f84581d157c7dbdb1f64e36
Ralitsa Todorova <rtodorova@intracol.com>
2017-11-03 08:26:48 -0400

    Formatting
    Signed-off-by: Ralitsa Todorova <rtodorova@intracol.com>

--- deployment/cloudbreak/params.py
+++ deployment/cloudbreak/params.py
@@ -15,35 +15,35 @@
 #   limitations under the License.
 #
 ##############################################################################
-kave_version="33-beta"
-cb_http_url="http://13.64.195.21"
-cb_https_url="https://13.64.195.21"
-uaa_port=8089
-cb_username="admin@example.com"
-cb_password="KavePassword01"
-ssl_verify=False
+kave_version = "33-beta"
+cb_http_url = "http://13.64.195.21"
+cb_https_url = "https://13.64.195.21"
+uaa_port = 8089
+cb_username = "admin@example.com"
+cb_password = "KavePassword01"
+ssl_verify = False
 recipes = {
-  "patchambari":
+    "patchambari":
     {
-      "recipeType": "PRE",
-      "description": "Add the KAVE Stack to ambari",
-      "templatePath" : "recipes/setup_cloudbreak_kavepatch_ambari.sh"
+        "recipeType": "PRE",
+        "description": "Add the KAVE Stack to ambari",
+        "templatePath": "recipes/setup_cloudbreak_kavepatch_ambari.sh"
     },
-  "fix-hosts-file": 
+    "fix-hosts-file":
     {
-      "recipeType": "PRE",
-      "description": "Fix hosts file on all nodes",
-      "templatePath" : "recipes/setup_cloudbreak_fixhostsfile_all.sh"
+        "recipeType": "PRE",
+        "description": "Fix hosts file on all nodes",
+        "templatePath": "recipes/setup_cloudbreak_fixhostsfile_all.sh"
     },
-  "distibute-private-key":
+    "distibute-private-key":
     {
-      "recipeType": "PRE",
-      "description": "Distribute private key on all nodes",
-      "templatePath" : "recipes/setup_cloudbreak_keydistrib_all.sh"
+        "recipeType": "PRE",
+        "description": "Distribute private key on all nodes",
+        "templatePath": "recipes/setup_cloudbreak_keydistrib_all.sh"
     },
-  "limit-ssh-attempts":
+    "limit-ssh-attempts":
     {
-      "recipeType": "PRE",
-      "description": "Limit unsuccessful ssh attempts rate",
-      "templatePath" : "recipes/limit-ssh-attempts.sh"
+        "recipeType": "PRE",
+        "description": "Limit unsuccessful ssh attempts rate",
+        "templatePath": "recipes/limit-ssh-attempts.sh"
     }}

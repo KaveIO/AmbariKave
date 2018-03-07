9e0edfcca755a710e78c231ec0de641ed19102c2
Ralitsa Todorova <rtodorova@intracol.com>
2018-03-07 07:43:17 -0500

    ABK-1284: Stack Advisor versions
    Update versions in stack_advisor.py according to the new release
    
    Signed-off-by: Ralitsa Todorova <rtodorova@intracol.com>

--- src/KAVE/services/stack_advisor.py
+++ src/KAVE/services/stack_advisor.py
@@ -18,7 +18,7 @@
 ##############################################################################
 
 
-class HDP2530KAVEStackAdvisor(HDP25StackAdvisor):
+class HDP2635KAVEStackAdvisor(HDP26StackAdvisor):
 
     # List of validators which should also be evaluated if there is not recommended default present.
     validateWithoutRecommendedDefault = ['freeipa']
@@ -31,7 +31,7 @@ class HDP2530KAVEStackAdvisor(HDP25StackAdvisor):
         return None
 
     def getServiceConfigurationValidators(self):
-        parentValidators = super(HDP2530KAVEStackAdvisor, self).getServiceConfigurationValidators()
+        parentValidators = super(HDP2635KAVEStackAdvisor, self).getServiceConfigurationValidators()
         childValidators = {
             "FREEIPA": {"freeipa": self.validateFreeIPAConfigurations}
         }

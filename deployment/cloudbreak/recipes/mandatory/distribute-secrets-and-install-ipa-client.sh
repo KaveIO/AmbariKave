988d7984645f4f1287a3d9f34891a60b71221e32
rvvincelli <Vincelli.Riccardo@KPMG.nl>
2018-03-14 12:24:27 +0100

    freeipa install refactoring

--- deployment/cloudbreak/recipes/mandatory/distribute-secrets-and-install-ipa-client.sh
+++ deployment/cloudbreak/recipes/mandatory/distribute-secrets-and-install-ipa-client.sh
@@ -27,42 +27,52 @@ list_of_secret_files="/root/admin-password
 /root/someadmin-password"
 
 #Get clustername
-cluster_name="$(curl -u ${ambari_user}:${ambari_password} -i \
-  -H 'X-Requested-By: ambari'  http://$ambari_host:$ambari_port/api/v1/clusters \
-  2>/dev/null | sed -n 's/.*"cluster_name" : "\([^\"]*\)".*/\1/p')"
+cluster_name () {
+    echo "$(curl -u ${ambari_user}:${ambari_password} -i \
+      -H 'X-Requested-By: ambari'  http://$ambari_host:$ambari_port/api/v1/clusters \
+      2>/dev/null | sed -n 's/.*"cluster_name" : "\([^\"]*\)".*/\1/p')"
+}
 
 #Get all hosts from ambari API
-hostlist=`curl -i -H "X-Requested-By: ambari" \
-  -u ${ambari_user}:${ambari_password} -X GET http://$ambari_host:$ambari_port/api/v1/hosts \
-  2>/dev/null | grep "host_name" | awk '{print $3}' | tr -d \"`
+hostlist () {
+    echo "$(curl -i -H "X-Requested-By: ambari" \
+      -u ${ambari_user}:${ambari_password} -X GET http://$ambari_host:$ambari_port/api/v1/hosts \
+    2>/dev/null | grep "host_name" | awk '{print $3}' | tr -d \")"
+}
 
-
-ambari_api_url="http://$ambari_host:$ambari_port/api/v1/clusters/$cluster_name"
+ambari_api_url () { 
+	echo http://$ambari_host:$ambari_port/api/v1/clusters/$(cluster_name) 
+}
 
 #Get host on which FreeIPA server is installed:
-eval freeipa_host="$(curl -u ${ambari_user}:${ambari_password} -i -H 'X-Requested-By: ambari'  \
-  $ambari_api_url/services/FREEIPA/components/FREEIPA_SERVER?fields=host_components/HostRoles/host_name \
-  2>/dev/null | grep "\"host_name\"" | awk '{print $3}')"
+freeipa_host () {
+    echo $(curl -u ${ambari_user}:${ambari_password} -i -H 'X-Requested-By: ambari'  \
+      $(ambari_api_url)/services/FREEIPA/components/FREEIPA_SERVER?fields=host_components/HostRoles/host_name \
+    2>/dev/null | grep "\"host_name\"" | awk '{print $3}' | tr -d \")
+}
 
 # fetch secret files from freeipa node, distribute them to all the rest.
-for filename in $list_of_secret_files
-do
-    echo `salt $freeipa_host cmd.run "cat $filename" --out txt | awk '{print $2}'` > $filename
-    chmod 600 $filename
-        for current_host in $hostlist
+function fetch_secret_files {
+    for filename in $list_of_secret_files
     do
-        if [ "$current_host" != "$freeipa_host" ]
-        then
-        salt-cp $current_host $filename $filename
-        fi
+    	until salt $(freeipa_host) cmd.run "ls $filename"; do echo sleep 60; done
+        echo `salt $(freeipa_host) cmd.run "cat $filename" --out txt | awk '{print $2}'` > $filename
+        chmod 600 $filename
+        for current_host in $(hostlist)
+        do
+            if [ "$current_host" != "$(freeipa_host)" ]
+            then
+                salt-cp $current_host $filename $filename
+            fi
+        done
     done
-done
+}
 
 #prepare csv list of hosts with freeipa server host excluded
 host_csv=""
-for current_host in $hostlist
+for current_host in $(hostlist)
    do
-        if [ "$current_host" != "$freeipa_host" ]
+        if [ "$current_host" != "$(freeipa_host)" ]
         then
         host_csv=$current_host","$host_csv
         fi
@@ -88,9 +98,21 @@ post_data="{
 }"
 
 #Add FREEIPA_CLIENT component to all hosts except FreeIPA server
-curl -i -H "X-Requested-By: ambari" -u ${ambari_user}:${ambari_password} \
-  -X POST -d "$post_data" "$ambari_api_url/hosts"
+function add_freeipa_client {
+    curl -i -H "X-Requested-By: ambari" -u ${ambari_user}:${ambari_password} \
+     -X POST -d "$post_data" "$(ambari_api_url)/hosts"
+}
   
 #Tell Amari to install the newly added component(s)  
-curl -i -H "X-Requested-By: ambari" -u ${ambari_user}:${ambari_password} \
-  -X PUT -d '{ "HostRoles": { "state":"INSTALLED" } }' "$ambari_api_url/host_components?HostRoles/state=INIT"
+function install_freeipa_client {
+    curl -i -H "X-Requested-By: ambari" -u ${ambari_user}:${ambari_password} \
+     -X PUT -d '{ "HostRoles": { "state":"INSTALLED" } }' "$(ambari_api_url)/host_components?HostRoles/state=INIT"
+}
+
+function main {
+    fetch_secret_files
+    add_freeipa_client
+    install_freeipa_client
+}
+
+main &
